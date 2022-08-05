4984cf4672941b281f356905272b3bf07ee5ccf3
PolinaLihane <polin@list.ru>
2022-08-05 22:44:56 +0300

    решили 0 задачу

---
+++ Example001_Square/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+// задача 0
+Clear();
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+int sqr = number * number;
+int sqr1 = Convert.ToInt32(Math.Pow(number,2));
+WriteLine($"Квадрат числа {number} = {Convert.ToInt32(Math.Pow(number,2))}");
+WriteLine($"Квадрат числа {number} = {sqr1}");
