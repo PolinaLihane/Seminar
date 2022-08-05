985827777d79756c3be5866f270bed16f6a27d39
PolinaLihane <polin@list.ru>
2022-08-05 23:08:20 +0300

    решила задачу с днем недели

---
+++ Example002_Week/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Write("Введите число: ");
+int day = Convert.ToInt32(ReadLine());
+if (day == 1) WriteLine("Понедельник");
+if (day == 2) WriteLine("Вторник");
+if (day == 3) WriteLine("Среда");
+if (day == 4) WriteLine("Четверг");
+if (day == 5) WriteLine("Пятница");
+if (day == 6) WriteLine("Суббота");
+if (day == 7) WriteLine("Воскресенье");
+else WriteLine("Ошибка");
