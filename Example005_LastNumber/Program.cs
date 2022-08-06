a57b81e862e52c92552c49e245c58805d3d4c207
PolinaLihane <polin@list.ru>
2022-08-06 16:35:16 +0300

    решила задачу с определением последней цифры

---
+++ Example005_LastNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите трехзначное число: ");
+int number = int.Parse(ReadLine());
+if (number>99 && number<1000)
+{
+    int LastNumber = number % 10;
+WriteLine($"Последняя цифра: {LastNumber}");
+}
+else WriteLine("Число не трехзначное");
+
+
