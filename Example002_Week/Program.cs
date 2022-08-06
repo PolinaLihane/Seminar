1023611e69a3a1ace1621e422ed164ebb2d7aa7d
PolinaLihane <polin@list.ru>
2022-08-06 13:46:34 +0300

    решила задачу с модулем числа

--- Example002_Week/Program.cs
+++ Example002_Week/Program.cs
@@ -2,11 +2,11 @@
 // день недели по числу, почему в конце ошибка?
 Write("Введите число: ");
 int day = Convert.ToInt32(ReadLine());
-if (day == 1) WriteLine("Понедельник");
-if (day == 2) WriteLine("Вторник");
-if (day == 3) WriteLine("Среда");
-if (day == 4) WriteLine("Четверг");
-if (day == 5) WriteLine("Пятница");
-if (day == 6) WriteLine("Суббота");
-if (day == 7) WriteLine("Воскресенье");
+if (day == 1) {WriteLine("Понедельник");}
+if (day == 2) {WriteLine("Вторник");}
+if (day == 3) {WriteLine("Среда");}
+if (day == 4) {WriteLine("Четверг");}
+if (day == 5) {WriteLine("Пятница");}
+if (day == 6) {WriteLine("Суббота");}
+if (day == 7) {WriteLine("Воскресенье");}
 else WriteLine("Ошибка");
---
+++ Example004_Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Add number: ");
+int number = int.Parse(ReadLine());
+int N = - number;
+while (N<=number) 
+{
+    Write($"{N}, ");
+N++;
+}
