a6e8c3a77b35e86a25295735449d78b284a05fba
PolinaLihane <polin@list.ru>
2022-08-12 13:48:43 +0300

    решили 9 задачу

---
+++ Example009_RandomNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+// 9. Найти число на отрезке, показать наибольшую цифру числа
+
+int GetRandomNumberInRange(int minNumber, int maxNumber)
+{
+    int result = new Random().Next(minNumber,maxNumber+1);
+    return result;  // возврат значения
+}
+int GetMaxDigitFromNumber(int number)
+{
+    int result =number/10;
+    int secondDigit = number % 10;
+    if (result < secondDigit)
+    {
+        result = secondDigit;
+    }
+return result;
+}
+for (int i = 0; i < 10; i++)
+{
+    int randomNumber = GetRandomNumberInRange(1,99);
+int maxDigit = GetMaxDigitFromNumber(randomNumber);
+WriteLine($"Наибольшая цифра числа{randomNumber} является {maxDigit}");
+}
