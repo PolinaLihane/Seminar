b573679027e55af295b4b09f1a2e154c7523b311
PolinaLihane <polin@list.ru>
2022-08-21 22:05:17 +0300

    почти сама решила задачи

--- Example35/Program.cs
+++ Example35/Program.cs
@@ -5,22 +5,35 @@ using static System.Console;
 Clear();
 WriteLine("Введите размер");
 int size = int.Parse(ReadLine());
-int count=0;
+// int count =0;
 int[] array = GetArray(size);
 WriteLine($"[{String.Join(",",array)}]");
-
-foreach(int item in array)
+int min = 10;
+int max = 99;
+int NewCount = FindNumbers(array,min,max);
+WriteLine(NewCount);
+//foreach(int item in array)
+//{
+  //  count+=item<99&&item>10?item:0;
+//}
+//WriteLine(count);
+int FindNumbers(int[] array,int min, int max)
 {
-    count+=item<99&&item>10?item:0;
+    int count = 0;
+    for (int i=0; i<array.Length;i++)
+    {
+        if (array[i] >= min && array[i] <= max) count++;
+    }
+    return count;
 }
-WriteLine(count);
+
 
 int[] GetArray(int size)
 {
     int[] result=new int[size];
     for (int i=0;i<size;i++)
     {
-        result[i]=new Random().Next(-999,999);
+        result[i]=new Random().Next(0,150);
     }
     return result;
 }
--- Example37/Program.cs
+++ Example37/Program.cs
@@ -2,3 +2,55 @@
 // Задать массив в указанном диапазоне и выдать поизведение пар чисел
 //1 и последний, 2 и предпоследний и тд
 //если кол-во не четное то центральный элемент не умножаем сам на себя
+
+using static System.Console;
+Clear();
+WriteLine("введите длину массива");
+int size = int.Parse(ReadLine());
+WriteLine("введите начало диапазона");
+int minValue = int.Parse(ReadLine());
+WriteLine("введите конец диапазона");
+int maxValue = int.Parse(ReadLine());
+int[] array = GetArray(size, minValue, maxValue);
+WriteLine($"[{String.Join(" ", array)}]");
+int[] NewArray = GetNewArray(array);
+WriteLine($"{String.Join(" ", NewArray)}");
+
+int[] GetNewArray(int[] array)
+{
+    int newsize = array.Length % 2 == 0 ? array.Length / 2 : array.Length / 2 + 1;
+    int[] result = new int[newsize];
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        result[i] = array[i] + array[array.Length - i - 1];
+    }
+   if (array.Length%2==1)
+   {
+    result[newsize-1]= array[array.Length/2];
+   }
+    return result;
+}
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
+//int[] GetNewArray(int[] array)
+//{
+   // int newsize = array.Length % 2 == 0 ? array.Length / 2 : array.Length / 2 + 1;
+  //  int[] result = new int[newsize];
+  //  for (int i = 0; i < array.Length; i++)
+   // {
+    //if (array[i]!=array[(array.Length-1)-i])
+   //   { result[i] = array[i] + array[(array.Length - 1)- i];}
+   // else result[i]=array[i]
+  //  }
+  // }
+//return result;
+//}
